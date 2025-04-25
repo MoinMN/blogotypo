ddfb37c93ff6464dc12c5a567ced2effdb676cf5
MoinMN <moinnaik45@eng.rizvi.edu.in>
2025-04-25 20:12:07 +0530

    date formatted

--- components/FormatDate.jsx
+++ components/FormatDate.jsx
@@ -1,24 +1,29 @@
-
 export const formatDateForBlog = (timestamp) => {
-  const date = new Date(timestamp);
+  const istDate = new Date(
+    new Date(timestamp).toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
+  );
 
-  const hours = date.getHours();
-  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const hours = istDate.getHours();
+  const minutes = istDate.getMinutes().toString().padStart(2, '0');
   const ampm = hours >= 12 ? 'pm' : 'am';
-  const formattedHours = hours % 12 || 12; // Convert 24-hour to 12-hour format
+  const formattedHours = hours % 12 || 12;
 
-  const day = date.getDate();
-  const month = date.toLocaleString('default', { month: 'long' }); // Get full month name
-  const year = date.getFullYear();
+  const day = istDate.getDate();
+  const month = istDate.toLocaleString('default', { month: 'long' });
+  const year = istDate.getFullYear();
 
   return `${formattedHours}:${minutes}${ampm}, ${day} ${month} ${year}`;
-}
-
+};
 
 // format date to mintus, days, date, etc
 export const formatDateForComment = (timestamp) => {
-  const now = new Date();
-  const postDate = new Date(timestamp);
+  const now = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
+  );
+  const postDate = new Date(
+    new Date(timestamp).toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
+  );
+
   const diffInSeconds = Math.floor((now - postDate) / 1000);
 
   if (diffInSeconds < 60) {
@@ -33,20 +38,21 @@ export const formatDateForComment = (timestamp) => {
     const days = Math.floor(diffInSeconds / 86400);
     return `${days} day${days > 1 ? "s" : ""} ago`;
   } else {
-    // Format the date as "YYYY-MM-DD" for posts older than 30 days
     return postDate.toISOString().split("T")[0];
   }
 };
 
 export const formatDateForAdmin = (timestamp) => {
-  const date = new Date(timestamp);
+  const istDate = new Date(
+    new Date(timestamp).toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
+  );
 
-  const hours = date.getHours().toString().padStart(2, '0'); // 24-hour format
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Month is 0-based
-  const year = date.getFullYear();
+  const hours = istDate.getHours().toString().padStart(2, '0');
+  const minutes = istDate.getMinutes().toString().padStart(2, '0');
+  const day = istDate.getDate().toString().padStart(2, '0');
+  const month = (istDate.getMonth() + 1).toString().padStart(2, '0');
+  const year = istDate.getFullYear();
 
   return `${hours}:${minutes}, ${day}/${month}/${year}`;
 };
+
