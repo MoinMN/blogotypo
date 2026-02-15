55cb2820ddfbb1e8399e0b233af03c7d720a8f35
MoinMN <moinnaik45@eng.rizvi.edu.in>
2026-02-15 13:58:06 +0530

    domain updated

--- app/sitemap.jsx
+++ app/sitemap.jsx
@@ -2,13 +2,13 @@ import Blog from "@models/blog";
 import connectMongoDB from "@utils/database";
 
 export default async function sitemap() {
-
+  const baseUrl = process.env.NEXT_PUBLIC_NEXTAUTH_URL;
   await connectMongoDB();
 
   const blogs = await Blog.find().select("title updatedAt");
 
   const blogUrls = blogs.map((blog) => ({
-    url: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}/blog/${encodeURIComponent(blog?.title?.split(" ").join("-"))}`,
+    url: `${baseUrl}/blog/${encodeURIComponent(blog?.title?.split(" ").join("-"))}`,
     lastModified: blog.updatedAt || new Date(),
     changeFrequency: "weekly",
     priority: 0.8,
@@ -16,7 +16,7 @@ export default async function sitemap() {
 
   return [
     {
-      url: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+      url: baseUrl,
       lastModified: new Date(),
       changeFrequency: "daily",
       priority: 1,
